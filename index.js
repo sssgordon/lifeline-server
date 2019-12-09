f0b90e57e69835c6879c460b6ab4670c5dcb89cf
sssgordon <gso7@uwo.ca>
2019-12-09 23:52:26 +0100

    Comment out profile scope

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get(
   "/auth/google",
   passport.authenticate("google", {
     scope: [
-      "https://www.googleapis.com/auth/userinfo.profile",
+      // "https://www.googleapis.com/auth/userinfo.profile",
       "https://www.googleapis.com/auth/userinfo.email"
       // "https://www.googleapis.com/auth/dialogflow"
     ]
