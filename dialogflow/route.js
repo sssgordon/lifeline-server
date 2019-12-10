88a5ede4816d5f733092170a5eb1d239aa99eb67
sssgordon <gso7@uwo.ca>
2019-12-10 15:41:04 +0100

    Dialogflow can recognize alias and response with user given name

--- dialogflow/route.js
+++ dialogflow/route.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { dialogflow, SignIn } = require("actions-on-google");
+const User = require("../user/model");
 
 const router = new Router();
 const app = dialogflow({
@@ -8,7 +9,7 @@ const app = dialogflow({
     "41211512517-iftbunpblmiv7qn22cl3itkiqtuvrssp.apps.googleusercontent.com" // I need to setup OAUTH for sign-up to work
 });
 
-app.intent("test", async (conv, params) => {
+app.intent("test", async conv => {
   // console.log("CONV TEST", conv);
   // console.log("PARAMS TEST", params);
   // send test email to himself
@@ -16,12 +17,21 @@ app.intent("test", async (conv, params) => {
   conv.close("Testing is a success!");
 });
 
-app.intent("declaration", async conv => {
-  // use params to get user name to match with db
-  // find user in db
-  // send email
-  // get user name
-  conv.close(`Your lawyer is on the way, hang in there.`);
+app.intent("declaration", async (conv, { name }) => {
+  try {
+    // use params to get user name to match with db
+    const user = await User.findOne({ where: { alias: name } });
+
+    if (user) {
+      const usergivenName = user.givenName;
+      conv.close(`Your lawyer is on the way, hang in there ${usergivenName}.`);
+    } else {
+      conv.ask("Try again?");
+    }
+    // send email
+  } catch (error) {
+    next(error);
+  }
 });
 
 //sign in intents
